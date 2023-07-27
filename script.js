351abbdb9f282b1b67fc193229384872cc9d212b
Ogbaudu Oghenemaro <maroprosper14@gmail.com>
2023-07-27 22:53:01 +0100

    Completed the project

--- script.js
+++ script.js
@@ -1,57 +1,118 @@
 let bookCase = [];
-let display = document.querySelector('.show-case');
-let i;
-function book(title, author, pages, readStatus){
+let index = 0;
+const display = document.querySelector('.show-case');
+const popBox = document.querySelector('.pop-up');
+function book(title, author, pages, readStatus, statusBtn, index){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.readStatus = readStatus;
+    this.statusBtn = statusBtn;
+    this.index = index;
+}
+book.prototype.changeStatus = function(){
+    if(this.readStatus === 'Read' && this.statusBtn === 'green'){
+        this.readStatus = 'Not Read';
+        this.statusBtn = 'red'
+    }
+    else{
+        this.readStatus = 'Read';
+        this.statusBtn = 'green'
+    }
+};
+book.prototype.updateIndex = function(){
+    this.index = 0;
+};
+function removeBook(index){
+    bookCase.splice(index, 1);
 }
 function addBook(e){
     const title = document.querySelector('#book-title').value;
     const author = document.querySelector('#book-author').value;
     const pages = document.querySelector('#book-pages').value;
     let readStatus = '';
+    let statusBtn = '';
     if(document.querySelector('#book-status').checked){
         readStatus = 'Read';
+        statusBtn = 'green';
     }
     else{
         readStatus = 'Not Read';
+        statusBtn = 'red';
     }
-    let books = new book(title, author, pages, readStatus);
+    let books = new book(title, author, pages, readStatus, statusBtn, index);
     bookCase.push(books);
-    for(i = 0; i< bookCase.length; i++){
-    } 
+    function displayBooks(){
+            const div = document.createElement('div');
+            const bookName = document.createElement('p');
+            const bookAuthor = document.createElement('p');
+            const bookPages = document.createElement('p');
+            const bookStatus = document.createElement('button');
+            const bookRemove = document.createElement('button');
+            {
+                bookName.textContent = `"${noteBook.title}"`;
+                div.appendChild(bookName);
+                bookName.classList.add('title');
+                bookAuthor.textContent = `${noteBook.author}`;
+                div.appendChild(bookAuthor);
+                bookAuthor.classList.add('author');
+                bookPages.textContent = `${noteBook.pages}`;
+                div.appendChild(bookPages);
+                bookPages.classList.add('pages');
+                bookStatus.textContent = `${noteBook.readStatus}`;
+                div.appendChild(bookStatus);
+                bookStatus.setAttribute('type', 'button');
+                bookStatus.addEventListener('click', (e) => {
+                    bookCase[parseInt(e.target.value)].changeStatus();
+                    display.innerHTML = '';
+                    for(noteBook of bookCase){
+                    displayBooks();
+                    }
+                });
+                bookStatus.classList.add('status');
+                bookStatus.style.backgroundColor = `${noteBook.statusBtn}`;
+                bookStatus.setAttribute('value', `${noteBook.index}`);
+                bookRemove.textContent = 'Remove';
+                div.appendChild(bookRemove);
+                bookRemove.classList.add('remove');
+                bookRemove.addEventListener('click', (e) => {
+                    removeBook(parseInt(e.target.value));
+                    display.innerHTML = '';
+                    for(let i = 0; i < bookCase.length; i++){
+                            bookCase[i].updateIndex();
+                            bookCase[i].index = i;
+                        }
+                        for(noteBook of bookCase){
+                            displayBooks();
+                        }
+                });
+                bookRemove.setAttribute('type', 'button');
+                bookRemove.setAttribute('value', `${noteBook.index}`);
+            }
+            display.appendChild(div);
+            div.classList.add('bookCard');
+        } 
     e.preventDefault();
-    console.log(bookCase);
+    display.innerHTML = '';
+    for(noteBook of bookCase){
+        displayBooks();
+    }
+    closePopUp();
+    index += 1;
+}
+function openPopUp(){
+        popBox.style.display = 'block';
+}
+function closePopUp(){
+        popBox.style.display = 'none';
+        clear();
+}
+function clear(){
+        document.querySelector('#book-title').value ='';
+        document.querySelector('#book-author').value ='';
+        document.querySelector('#book-pages').value = '';
+        document.querySelector('#book-status').checked = false;
 }
 document.querySelector('#submit-book').addEventListener('click', addBook, false);
-function displayBooks(){
-    let div = document.createElement('div');
-        {
-            let bookName = document.createElement('p');
-            bookName.textContent = `"${bookCase[i].title}"`;
-            div.appendChild(bookName);
-            bookName.classList.add('title');
-            let bookAuthor = document.createElement('p');
-            bookAuthor.textContent = `${bookCase[i].author}`;
-            div.appendChild(bookAuthor);
-            bookAuthor.classList.add('author');
-            let bookPages = document.createElement('p');
-            bookPages.textContent = `${bookCase[i].pages}`;
-            div.appendChild(bookPages);
-            bookPages.classList.add('pages');
-            let bookStatus = document.createElement('button');
-            bookStatus.textContent = `${bookCase[i].readStatus}`;
-            div.appendChild(bookStatus);
-            bookStatus.setAttribute('type', 'button');
-            bookStatus.classList.add('status');
-            let bookRemove = document.createElement('button');
-            bookRemove.textContent = 'Remove';
-            div.appendChild(bookRemove);
-            bookRemove.classList.add('remove');
-            bookRemove.setAttribute('type', 'button');
-        }
-        display.appendChild(div);
-        div.classList.add('bookCard');
-    }
+document.querySelector('#btn-popUp').addEventListener('click', openPopUp);
+document.querySelector('#close-pop').addEventListener('click', closePopUp);
